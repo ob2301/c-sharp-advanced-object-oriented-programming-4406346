e6977fb2ab242421ded41aadf103ee3149b7ada5
Omar <omarcopyb@gmail.com>
2026-01-05 01:29:12 +0000

    Test 2

--- Start/Ch1/PropsMethods/Program.cs
+++ Start/Ch1/PropsMethods/Program.cs
@@ -9,5 +9,13 @@ Console.WriteLine(rect2.GetArea());
 
 // TODO: Operate on the Properties
 
+rect1.BorderSize = 5;
+Console.WriteLine(rect1.BorderSize);
+rect1.Width = 15;   
+Console.WriteLine(rect1.Width);
+rect1.Height = 25;
+Console.WriteLine(rect1.Height);
+Console.WriteLine(rect1.GetArea());
+
 
 // TODO: Try setting an invalid value
